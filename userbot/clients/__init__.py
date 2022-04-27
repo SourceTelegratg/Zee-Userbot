93db175bff6e1d99af0e2c8f2695386a288116ae
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2022-04-27 22:42:48 +0300

    Add files via upload

---
+++ userbot/clients/__init__.py
@@ -0,0 +1,10 @@
+# Zee - UserBot
+# Copyright (c) 2022 Zee-Userbot
+# Credits: @zeafeya || https://github.com/Zed-Thon
+#
+# This file is a part of < https://github.com/Zed-Thon/Zee-Userbot/ >
+# t.me/Dbzea & t.me/Storezeastore
+
+from .client_list import client_id, clients_list
+from .logger import man_userbot_on
+from .startup import man_client, multiman
